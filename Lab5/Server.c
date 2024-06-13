61438b54679b441ed5b5809147ce70fc899f2407
CLRN2001 <166783591+CLRN2001@users.noreply.github.com>
2024-06-13 17:13:44 -0400

    Server.c

--- Lab5/Server.c
+++ Lab5/Server.c
@@ -3,30 +3,17 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <signal.h>
 
 #define PUERTO 8080
 #define BUF_SIZE 1024
 
-int server_fd;
-
-void handle_sigint(int sig) {
-    printf("\nCerrando el servidor...\n");
-    if (server_fd != -1) {
-        close(server_fd);
-    }
-    exit(0);
-}
-
 int main() {
     struct sockaddr_in address;
+    int server_fd;
     int opt = 1;
     int addrlen = sizeof(address);
     char buffer[BUF_SIZE] = {0};
     const char *mensaje = "Bienvenido al servidor\n";
-
-    // Registrar el manejador de señales
-    signal(SIGINT, handle_sigint);
 
     // Crear el socket
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
@@ -43,7 +30,7 @@ int main() {
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+    address.sin_port = htons(PUERTO);
 
     // Vincular el socket al puerto especificado
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
@@ -53,35 +40,32 @@ int main() {
     }
 
     // Escuchar conexiones entrantes
-    if (listen(server_fd, 1) < 0) {  // Limitar la cola de conexiones a 1
+    if (listen(server_fd, 1) < 0) {
         perror("Error en listen");
         close(server_fd);
         exit(EXIT_FAILURE);
     }
 
-    printf("Servidor escuchando en el puerto %d\n", PORT);
+    printf("Servidor escuchando en el puerto %d\n", PUERTO);
 
-    while (1) {
-        int new_socket;
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-            perror("Error en accept");
-            continue;  // Continuar aceptando nuevas conexiones
-        }
-
-        // Enviar mensaje de bienvenida al cliente
-        send(new_socket, mensaje, strlen(mensaje), 0);
-        printf("Mensaje de bienvenida enviado\n");
-
-        // Leer el mensaje del cliente
-        int valread = read(new_socket, buffer, BUF_SIZE);
-        if (valread > 0) {
-            printf("Mensaje recibido del cliente: %s\n", buffer);
-        }
-
-        // Cerrar el socket del cliente
-        close(new_socket);
+    int new_socket;
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0){
+	perror("accept");
+	close(server_fd);
+	exit(EXIT_FAILURE);
+	}
+    // Leer el mensaje del cliente
+    int valread = read(new_socket, buffer, BUF_SIZE);
+    if (valread > 0) {
+        printf("Mensaje recibido del cliente: %s\n", buffer);
     }
 
+    // Enviar mensaje de vuelta
+    send(new_socket, buffer, BUF_SIZE, 0);
+    printf("Mensaje de vuelta enviado\n");
+
+    // Cerrar los sockets
+    close(new_socket);
     close(server_fd);
     return 0;
 }
